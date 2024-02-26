679d99b52a1a0bddd326b23c93eeb73d7c4eeff3
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2024-02-26 19:45:09 +0530

    Added Area of Circle program :Day2

---
+++ Day2/AreaOfCircle.cpp
@@ -0,0 +1,13 @@
+// Write a program to Print Area of Circle
+#include <iostream>
+using namespace std;
+int main()
+{
+	int radius;
+	cout<<"Enter radius of Circle "<<endl;
+	cin>>radius;
+	double area = 3.14*(radius*radius);
+	cout<<"Area of Circle "<<area;
+	return 0;
+}
+
